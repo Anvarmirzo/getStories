dddea0e2ec1e1e23ceda6afceb243c0ee2a46829
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2021-09-06 02:37:34 +0500

    cleared from unused files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const URL = 'https://instagram40.p.rapidapi.com';
+	const headers = {
+		'x-rapidapi-host': 'instagram40.p.rapidapi.com',
+		'x-rapidapi-key': 'a906d9718fmshf24956fbdbd7605p1d855cjsnf409f03e54de',
+	};
+
+	const [userName, setUserName] = useState('');
+
+	const formSubmitHandler = (e) => {
+		e.preventDefault();
+		axios
+			.get(`${URL}/account-info`, {
+				params: { username: userName },
+				headers,
+			})
+			.then(function (response) {
+				return response.data.id;
+			})
+			.then((id) =>
+				axios.get(`${URL}/stories`, {
+					params: { userid: id },
+					headers,
+				}),
+			)
+			.then(function (response) {
+				console.log(response.data);
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	};
+
+	const inputChangeHandler = (e) => {
+		setUserName(e.target.value);
+	};
+
+	return (
+		<div className='App'>
+			<form onSubmit={(e) => formSubmitHandler(e)}>
+				<input
+					type='text'
+					value={userName}
+					onChange={(e) => inputChangeHandler(e)}
+				/>
+				<button>Check</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
